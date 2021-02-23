55ef1912437d05b6f0894bb4749fd5b5b5aa2549
Daniel <Daniel.Vaskevic@wealth-dynamix.com>
2021-02-23 13:58:54 +0200

    Route
    Routes + NotFound(If)

--- starting/TheCodeCamp/Controllers/CampsController.cs
+++ starting/TheCodeCamp/Controllers/CampsController.cs
@@ -8,6 +8,8 @@ using TheCodeCamp.Models;
 
 namespace TheCodeCamp.Controllers
 {
+    // All methods will start with
+    [RoutePrefix("api/camps")]
     public class CampsController : ApiController
     {
         private readonly ICampRepository _repository;
@@ -18,10 +20,7 @@ namespace TheCodeCamp.Controllers
             _repository = repository;
             _mapper = mapper;
         }
-       
-        
-
-
+        [Route()]
         public async Task<IHttpActionResult> Get()
         {
             try
@@ -34,6 +33,22 @@ namespace TheCodeCamp.Controllers
                 return Ok(mappedResult);
             }
             catch(Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        // localhost:6060/api/camps + /{moniker}
+        [Route("{moniker}")]
+        public async Task<IHttpActionResult> Get(string moniker)
+        {
+            try
+            {
+                var result = await _repository.GetCampAsync(moniker);
+                if (result == null){ return NotFound(); }
+                return Ok(_mapper.Map<CampModel>(result));
+            }
+            catch (Exception ex)
             {
                 return InternalServerError(ex);
             }
