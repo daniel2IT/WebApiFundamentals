00a6b433ae8c419ea633112583ac54f08add1534
Daniel <Daniel.Vaskevic@wealth-dynamix.com>
2021-02-24 14:25:10 +0200

    Update Talk +  ->> Update Speaker ID
    Update  + Speaker ID
    JSON ::::
    {
        "title": "new Talk ",
        "abstract": "new Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talknew Daniel Talk",
        "level": 200,
        "speaker":{
            "speakerId": 1
        }
    }

--- starting/TheCodeCamp/Controllers/TalksController.cs
+++ starting/TheCodeCamp/Controllers/TalksController.cs
@@ -32,7 +32,7 @@ namespace TheCodeCamp.Controllers
 
                 return Ok(_mapper.Map<IEnumerable<TalkModel>>(results));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return InternalServerError(ex);
             }
@@ -49,7 +49,7 @@ namespace TheCodeCamp.Controllers
 
                 return Ok(_mapper.Map<TalkModel>(result));
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return InternalServerError(ex);
             }
@@ -64,13 +64,13 @@ namespace TheCodeCamp.Controllers
                 if (ModelState.IsValid)
                 {
                     var camp = await _repository.GetCampAsync(moniker);
-                    if(camp != null)
+                    if (camp != null)
                     {
                         var talk = _mapper.Map<Talk>(model);
                         talk.Camp = camp;
 
                         //Map the Speaker if Necessary
-                        if(model.Speaker != null)
+                        if (model.Speaker != null)
                         {
                             var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
                             if (speaker != null) talk.Speaker = speaker;
@@ -79,23 +79,61 @@ namespace TheCodeCamp.Controllers
 
                         _repository.AddTalk(talk);
 
-                        if(await _repository.SaveChangesAsync())
+                        if (await _repository.SaveChangesAsync())
                         {
-                            return CreatedAtRoute("GetTalk", 
-                                new { moniker = moniker, id = talk.TalkId }, 
+                            return CreatedAtRoute("GetTalk",
+                                new { moniker = moniker, id = talk.TalkId },
                                 _mapper.Map<TalkModel>(talk));
                         }
-                        
+
                     }
-                   
+
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 return InternalServerError(ex);
             }
 
             return BadRequest(ModelState);// ModelState - Contains all Validation Errors
         }
+
+        //Put
+        [Route("{id:int}")]
+        public async Task<IHttpActionResult> Put(string moniker, int talkId,
+            TalkModel model)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    var talk = await _repository.GetTalkByMonikerAsync(moniker, talkId, true);
+                    if (talk == null) return NotFound();
+
+                    // it's going to ignore the Speaker 
+                    _mapper.Map(model, talk);
+
+                    // Change speaker if needed
+                    //talk.Speaker.SpeakerId - Existing ID
+                    // model.Speaker.SpeakerId - passed ID
+                    if (talk.Speaker.SpeakerId != model.Speaker.SpeakerId)
+                    {
+                        var speaker = await _repository.GetSpeakerAsync(model.Speaker.SpeakerId);
+                        if (speaker != null) talk.Speaker = speaker;
+                    }
+
+                    if(await _repository.SaveChangesAsync())
+                    {
+                        return Ok(_mapper.Map<TalkModel>(model));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+
+            return BadRequest(ModelState);
+        } 
     }
 }
--- starting/TheCodeCamp/Data/CampMappingProfile.cs
+++ starting/TheCodeCamp/Data/CampMappingProfile.cs
@@ -12,7 +12,9 @@ namespace TheCodeCamp.Data
                 .ForMember(c => c.Venue, opt => opt.MapFrom(m => m.Location.VenueName))
                 .ReverseMap();
 
-            CreateMap<Talk, TalkModel>().ReverseMap();
+            CreateMap<Talk, TalkModel>().ReverseMap()
+                .ForMember(t => t.Speaker, opt => opt.Ignore())
+                .ForMember(t => t.Camp, opt => opt.Ignore());
 
             CreateMap<Speaker, SpeakerModel>().ReverseMap();
         }
