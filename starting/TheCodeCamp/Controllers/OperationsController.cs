5da0dd79ee07cacedaf264853446aadd0cf2b0af
Daniel <Daniel.Vaskevic@wealth-dynamix.com>
2021-02-24 15:07:11 +0200

    Funtional Options
    Funtional ->> Here created for Refresh Configuration ..

---
+++ starting/TheCodeCamp/Controllers/OperationsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using System.Web.Http;
+
+namespace TheCodeCamp.Controllers
+{
+    public class OperationsController : ApiController
+    {
+        /* Reloading configuration without
+         * restarting application using
+         * ConfigurationManager.RefreshSection*/
+        [HttpOptions]
+        [Route("api/refreshconfig")]
+        public IHttpActionResult RefreshAppSettings()
+        {
+            try
+            {
+                ConfigurationManager.RefreshSection("AppSettings");
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+    }
+}
